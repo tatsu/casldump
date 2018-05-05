61661622d37246528fbf9e7e137fb230dd1718ee
Tatsuhiko Arai <tatsu@tatsu.com>
2018-05-05 14:46:41 +0900

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:casldump/casldump.dart' as casldump;
+
+main(List<String> arguments) {
+  print('Hello world: ${casldump.calculate()}!');
+}
---
+++ test/casldump_test.dart
@@ -0,0 +1,8 @@
+import 'package:casldump/casldump.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
